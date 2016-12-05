06f2d8a1b1cd1286ca89c28bc3bc5afb0716df27
tafani <ratafani@gmail.com>
2016-12-05 12:38:01 +0100

    starting

--- app/src/main/java/ba/edu/ibu/myapplication/home.java
+++ app/src/main/java/ba/edu/ibu/myapplication/home.java
@@ -1,7 +1,9 @@
 package ba.edu.ibu.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class home extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void goStart(View view) {
+        Intent goStart = new Intent(home.this,Starting.class);
+        startActivity(goStart);
     }
 }
