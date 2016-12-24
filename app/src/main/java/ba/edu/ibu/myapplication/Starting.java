ebb5dacfacac117bf29e484fa21b600ec0150989
tafani <ratafani@gmail.com>
2016-12-24 21:18:12 +0100

    puting string-array for data

--- app/src/main/java/ba/edu/ibu/myapplication/Starting.java
+++ app/src/main/java/ba/edu/ibu/myapplication/Starting.java
@@ -12,7 +12,7 @@ public class Starting extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_starting);
+        setContentView(R.layout.getting_info);
 
     }
 
