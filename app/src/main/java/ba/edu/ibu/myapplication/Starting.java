7a6c866479970857e280ddcb79e97d43109297ec
tafani <ratafani@gmail.com>
2016-12-25 17:53:26 +0100

    autoCompleteText for City and parsing array

--- app/src/main/java/ba/edu/ibu/myapplication/Starting.java
+++ app/src/main/java/ba/edu/ibu/myapplication/Starting.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class Starting extends AppCompatActivity {
 
@@ -13,7 +15,10 @@ public class Starting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.getting_info);
-
+        String[] city = getResources().getStringArray(R.array.City);
+        AutoCompleteTextView destin = (AutoCompleteTextView) findViewById(R.id.destination_city);
+        assert destin != null;
+        destin.setAdapter(new ArrayAdapter<String>(this, R.layout.list_detail, city));
     }
 
 }
